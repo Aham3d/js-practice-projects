ba113c3dfcd1d0b8bbe201ab0a27729f861df72e
Moin Ahamed <ahamedjust@gmail.com>
2023-02-13 20:00:36 +0530

    first commit

---
+++ 01-random-color-generator/setup/app.js
@@ -0,0 +1,15 @@
+const colors = ["green", "lightgreen", "red", "black", "gray", "blue", "lightblue", "yellow", "brown"];
+
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
+
+btn.addEventListener('click', function() {
+    const randomNumber = getRandomNumber(); 
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
+    color.style.color = colors[randomNumber];
+});
+function getRandomNumber() {
+    return Math.floor(Math.random() * colors.length);
+};
+
