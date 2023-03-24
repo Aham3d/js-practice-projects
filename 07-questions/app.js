7769fe89c907f985f16f17bd2110ac0b347b0f48
Moin Ahamed <ahamedjust@gmail.com>
2023-03-24 12:20:51 +0530

    going through

--- 07-questions/app.js
+++ 07-questions/app.js
@@ -1,4 +1,5 @@
 //using selectors inside the element
+
 const questions = document.querySelectorAll('.question');
 questions.forEach(function(question) {
     const btn = question.querySelector('.question-btn')
@@ -16,13 +17,8 @@ questions.forEach(function(question) {
 
 
 
-
-
-
-
 // traversing the dom
 // const btns = document.querySelectorAll('.question-btn');
-
 // btns.forEach(function(btn) {
 //     btn.addEventListener('click', function(e) {
 //         const question = e.currentTarget.parentElement.parentElement;
