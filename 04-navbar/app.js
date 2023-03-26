7589f300e4afe52ce0fbc86e051dab3968e4a479
Moin Ahamed <ahamedjust@gmail.com>
2023-03-26 16:14:22 +0530

    reviewed

--- 04-navbar/app.js
+++ 04-navbar/app.js
@@ -9,11 +9,11 @@ const navToggle = document.querySelector('.nav-toggle');
 const links = document.querySelector('.links');
 
 navToggle.addEventListener('click', function() {
-    // console.log(links.classList.contains('links'));
-    // if(links.classList.contains("show-links")) {
-    //     links.classList.remove("show-links");
-    // } else {
-    //     links.classList.add('show-links');
-    // }
+    console.log(links.classList.contains('links'));
+    if(links.classList.contains("show-links")) {
+        links.classList.remove("show-links");
+    } else {
+        links.classList.add('show-links');
+    }
     links.classList.toggle('show-links');
 });
