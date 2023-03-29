53b461d03685c9de5a2390c4907fbb2dd008496f
Moin Ahamed <ahamedjust@gmail.com>
2023-03-29 11:37:05 +0530

    tweeks

--- 07-questions/app.js
+++ 07-questions/app.js
@@ -1,15 +1,14 @@
 //using selectors inside the element
-
 const questions = document.querySelectorAll('.question');
 questions.forEach(function(question) {
     const btn = question.querySelector('.question-btn')
     btn.addEventListener('click', function() {
 
-        questions.forEach(function(item) {
-            if(item !== question) {
-                item.classList.remove('show-text');
-            } 
-        });
+        // questions.forEach(function(item) {
+        //     if(item !== question) {
+        //         item.classList.remove('show-text');
+        //     } 
+        // });
 
         question.classList.toggle('show-text');
     });
