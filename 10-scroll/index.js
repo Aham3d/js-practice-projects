d100a06ffa614501b2e1ce1d891b1172e4f8bf09
Moin Ahamed <ahamedjust@gmail.com>
2023-03-31 23:05:52 +0530

    re-check?

---
+++ 10-scroll/index.js
@@ -0,0 +1,67 @@
+const date = document.getElementById('date');
+date.innerHTML = new Date().getFullYear();
+
+const navToggle = document.querySelector(".nav-toggle");
+const linksContainer = document.querySelector('.links-container');
+const links = document.querySelector('.links');
+
+navToggle.addEventListener("clikc", function() {
+    linksContainer.classList.toggle("show-links");
+});
+
+
+// navbar
+const navbar = document.getElementById("nav");
+const topLink = document.querySelector(".top-link");
+
+window.addEventListener("scroll", function () {
+  const scrollHeight = window.pageYOffset;
+  const navHeight = navbar.getBoundingClientRect().height;
+  if (scrollHeight > navHeight) {
+    navbar.classList.add("fixed-nav");
+  } else {
+    navbar.classList.remove("fixed-nav");
+  }
+  // setup back to top link
+
+  if (scrollHeight > 500) {
+    console.log("helo");
+
+    topLink.classList.add("show-link");
+  } else {
+    topLink.classList.remove("show-link");
+  }
+});
+
+
+
+
+// smooth scroll 
+const scrollLinks = document.querySelectorAll('.scroll.links');
+scrollLinks.forEach((link) => {
+    link.addEventListener('click', (e) => {
+        e.preventDefault();
+
+        const id = e.currentTarget.getAttribute('href').slice(1);
+        const element = document.getElementById(id); 
+
+        const navHeight = navbar.getBoundingClientRect().height; 
+        const containerHeight = linksContainer.getBoundingClientRect().height;
+        const fixedNav = navbar.classList.contains("fixed-nav");
+        let position = element.offsetTop - navHeight;    
+
+        if(!fixedNav) {
+            position = position - navHeight; 
+        } 
+        if(navHeight > 82) {
+            position = position + containerHeight; 
+        }
+
+        window.scrollTo ({
+            left:0, 
+            top: position,
+        });
+
+        linksContainer.style.height = 0; 
+    });
+});
