218264c5ec447325f46cc460dfd3ef695cb8189a
Moin Ahamed <ahamedjust@gmail.com>
2023-03-31 23:53:03 +0530

    first

---
+++ 11-tabs/index.js
@@ -0,0 +1,23 @@
+const about = document.querySelector(".about");
+const btns = document.querySelector(".tab-btn");
+const articles = document.querySelector(".content");
+
+about.addEventListener('click', function(e) {
+    const id = e.target.dataset.id;
+    if(id) {
+        btns.forEach(function (btn) {
+            btn.classList.remove("active");
+        });
+
+        e.target.classList.add("active");
+
+        articles.forEach(function (article) {
+            article.classList.remove("active");
+          });
+
+          const element = document.getElementById(id);
+          element.classList.add("active");
+    
+      
+    }
+});
